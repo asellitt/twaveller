2247eb85d6d82aa6d8b520319d06a21ceab3345b
Anthony Sellitti <anthony.sellitti@envato.com>
2014-11-15 14:54:30 +1100

    root -> new trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :days
+  root 'trips#new'
 
+  resources :days
   resources :trips
 
   # The priority is based upon order of creation: first created -> highest priority.
