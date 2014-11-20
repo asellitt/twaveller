8a236261153a4b04eca0373b03ffa65e56f522af
Anthony Sellitti <anthony.sellitti@envato.com>
2014-11-20 22:44:12 +1100

    fix nil references

--- app/presenters/area_presenter.rb
+++ app/presenters/area_presenter.rb
@@ -7,7 +7,7 @@ class AreaPresenter
   end
 
   def proposed_date
-    area.proposed_date.strftime('%A, %e %B %Y')
+    area.proposed_date.strftime('%A, %e %B %Y') if area.proposed_date.present?
   end
 
   def total
@@ -20,7 +20,7 @@ class AreaPresenter
       gsub("\n", '<br/>').
       # convert links into ..links!
       gsub(/(http[s]?:\/\/[^\s]*)/, '<a href="\1">\1</a>').
-      html_safe
+      html_safe if area.description.present?
   end
 
 private
