efe4c53d31fa404e5618cf6d364961da87a7254c
Anthony Sellitti <anthony.sellitti@envato.com>
2015-10-15 21:45:31 +1100

    disambiguate what currency is displayed

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -7,3 +7,8 @@ Money::Bank::GoogleCurrency.ttl_in_seconds = 86400
 
 # set default bank to instance of GoogleCurrency
 Money.default_bank = Money::Bank::GoogleCurrency.new
+
+# default format
+Money.default_formatting_rules = {
+  with_currency: true
+}
