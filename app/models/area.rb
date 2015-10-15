a181c08f128294eb6a0954ce9c46733382101af4
Anthony Sellitti <anthony.sellitti@envato.com>
2015-10-15 21:45:15 +1100

    special validator to ensure google currency knows about the...currency...

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,6 @@
 class Area < ActiveRecord::Base
   has_many :attractions, dependent: :destroy
+  validates_with CurrencyCodeValidator
 
   def total
     attractions.collect(&:cost).sum.exchange_to(currency_code)
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ActiveRecord::Base
   has_many :areas, dependent: :destroy
   has_many :trip_rights, dependent: :destroy
+  validates_with CurrencyCodeValidator
 
   def total
     areas.collect(&:total).sum.exchange_to(currency_code)
